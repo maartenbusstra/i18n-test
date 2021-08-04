f492222c56029c5d81a2dd879df8c07785f8a825
Maarten Busstra <busstra.m@gmail.com>
2021-08-04 02:48:48 +0200

    restructure

--- merge.js
+++ merge.js
@@ -1,4 +1,19 @@
-const base = require("./base/en.json");
-const web = require("./web/en.json");
+const nodes = {
+  "/": require("./i18n/en.json"),
+  "/app": require("./i18n/app/en.json"),
+  "/app/mobile": require("./i18n/app/mobile/en.json"),
+  "/app/mobile/ios": require("./i18n/app/mobile/ios/en.json"),
+}
 
-console.log({ ...base, ...web });
+const translations = {}
+
+Object.keys(nodes).forEach((path) => {
+  const lang = nodes[path]
+  Object.keys(lang).forEach((id) => {
+    const baseId =
+      path === "/" ? "global" : path.replace("/", "").replace(/\//g, ".")
+    translations[`${baseId}.${id}`] = lang[id]
+  })
+})
+
+console.log(translations)
